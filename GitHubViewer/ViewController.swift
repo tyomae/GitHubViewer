9e31b9e6e0d7d48d1840a715764e0c4786acfda6
Artem <dom1nuk@yandex.ru>
2019-11-14 21:17:45 +0300

    Initial Commit

---
+++ GitHubViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubViewer
+//
+//  Created by Артем  Емельянов  on 14/11/2019.
+//  Copyright © 2019 Artem Emelianov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
